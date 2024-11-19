6a2f56a4bda1f646953470f1c839a69fe601548c
unknown <saidulupallapati999@gmail.com>
2024-11-19 23:19:22 +0530

    react-project

---
+++ react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+export default class FooterComponent extends Component {
+  render() {
+    return (
+      <div>
+        <footer className='footer'>
+            <span>All Rights Reserved-2024 &copy; SAIDULU</span>
+        </footer>
+        
+      </div>
+    )
+  }
+}
---
+++ react-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class HeaderComponent extends Component {
+  render() {
+    return (
+      <div>
+        <nav className='navbar  navbar-expand-lg bg-dark  navbar-dark'>
+            <div className='container'>
+                <a href="" className='navbar-brand'>Employee Management system</a>
+            </div>
+
+        </nav>
+      </div>
+    )
+  }
+}
